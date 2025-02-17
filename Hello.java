17f38925667e6a383c9681bea1538ffc1dc0f14c
CheekonMTN <112905139+CheekonMTN@users.noreply.github.com>
2025-02-17 16:12:35 -0500

    please good grade

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 Public Class Hello{
     public static void main( String argv[] )
      {
-     System.out.println( "Hello, " + argv[0] + "!" );
+     System.out.println( "Hello, " + argv[0] + "CSCI4485, Michael did this" );
     }
 }
